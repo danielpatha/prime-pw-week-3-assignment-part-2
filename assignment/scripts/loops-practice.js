f7dd5a4f334d85e9e40a77d3e289ab111b149ba5
Daniel Pathammavong <patha037@umn.edu>
2022-09-03 14:40:05 -0500

    Adding for-of and while loops with stars variable

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -42,12 +42,20 @@ let stars = ['Polaris', 'Gacrux', 'Formalhaut', 'Rigel', 'Deneb']
 
 // 2.a. TODO: Write a for of loop to console.log each star in the 'stars' array
 console.log('Some stars:');
+for(star of stars){
+  console.log(star);
+}
 
 // 3. While loops  
 console.log('---- 3. While loop ----');
 
 // 3.a. TODO: Write a while loop to console.log each star in the 'stars' array
 console.log('Some stars using while:');
+let p = 0;
+while(p < stars.length){
+console.log(stars[p]);
+p++;
+}
 
 // 3.b. TODO: Write a while loop to console.log the numbers from 0 to 5
 console.log('count from 0 to 5');
